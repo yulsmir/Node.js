f371b0bf52a6fea0ab3bf66c0eef1c1cdbfa6fff
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-05-08 15:54:03 +0200

    Add state, add, subtract, reset test coverage

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -9,12 +9,40 @@ function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    const data = {
+      state: state,
+    };
+
+    if (request.method === 'GET') {
+      if (request.url === '/state') {
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify(data));
+        // return state;
+      } else if (request.url === '/add') {
+        data.state++;
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify(data));
+        // return state;
+      } else if (request.url === '/subtract') {
+        data.state--;
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify(data));
+        // return state;
+      } else if (request.url === '/reset') {
+        data.state = 10;
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify(data));
+        // return state;
+      } else {
+        response.writeHead(404, { 'Content-Type': 'application/json' });
+        response.end(`${response.statusCode} Not Found`);
+      }
+    }
   });
 
   return server;
 }
 
 module.exports = {
-  createServer
+  createServer,
 };
